7f0453209c3560d9f407f2160d465e9c810cb51b
TanDo <tando98ptit@gmail.com>
2024-09-20 13:46:26 +0700

    down grade python version to 3.7

--- starter/models.py
+++ starter/models.py
@@ -6,12 +6,15 @@ database_path = DATABASE_URL
 
 db = SQLAlchemy()
 
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
-    db.init_app(app)
-    db.create_all()
+    with app.app_context():
+        db.app = app
+        db.init_app(app)
+        db.create_all()
+
 
 class Movie(db.Model):
     __tablename__ = 'movies'
